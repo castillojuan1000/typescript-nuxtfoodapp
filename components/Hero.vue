24c18c9d6a984960a158a80781e9483d6e26e977
Juan Castillo <castillojuan1000@gmail.com>
2021-01-14 17:50:21 -0500

    setup and hero page

---
+++ components/Hero.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="masthead" role="img" aria-label="Image Description">
+    <h1>RestaurantHub</h1>
+    <button>See Restaurants</button>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+.masthead {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  padding: 40px;
+  width: 100%;
+  height: 80vh; /* if you don't want it to take up the full screen, reduce this number */
+  overflow: hidden;
+  background-size: cover !important;
+  background: radial-gradient(
+      ellipse at center,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0) 62%,
+      rgba(0, 0, 0, 0.65) 100%
+    ),
+    url(~assets/headerimg.jpg) no-repeat center center scroll;
+}
+
+h1 {
+  font-style: normal;
+  font-weight: bold;
+  color: #98ce00;
+  font-size: 12vmin;
+  letter-spacing: 0.03em;
+  line-height: 1;
+  text-shadow: 1px 2px 4px rgba(0, 0, 0, 0.8);
+  margin-bottom: 32px;
+}
+</style>
