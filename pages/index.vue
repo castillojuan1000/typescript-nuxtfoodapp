e10e76802fbccf31459b118897527dc0334918b5
Juan Castillo <castillojuan1000@gmail.com>
2021-01-18 12:05:53 -0500

    home restaurantsInfo component done

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,23 @@
 <template>
   <main class="container">
     <Hero />
+    <RestaurantsInfo :dataSource="foodData" />
   </main>
 </template>
 
 <script lang="ts">
 import Hero from "@/components/Hero.vue";
+import RestaurantsInfo from "@/components/RestaurantsInfo.vue";
+import { mapState } from "vuex";
 import Vue from "vue";
 
 export default Vue.extend({
   components: {
     Hero,
+    RestaurantsInfo,
+  },
+  computed: {
+    ...mapState(["foodData"]),
   },
 });
 </script>
