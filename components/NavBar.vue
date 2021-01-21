abb12ad0427d72365aa8f344f64c2fb8199b1af5
Juan Castillo <castillojuan1000@gmail.com>
2021-01-21 11:26:05 -0500

    nav home icon and cartCount added

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,18 +2,34 @@
   <nav>
     <ul>
       <li>
-        <nuxt-link exact to="/">Home</nuxt-link>
+        <nuxt-link exact to="/"><Logo /></nuxt-link>
       </li>
       <li><nuxt-link to="/restaurants">Restaurants</nuxt-link></li>
     </ul>
+    <div class="smallnum" v-if="cartCount > 0">{{ cartCount }}</div>
     <nuxt-link to="/cart">Cart</nuxt-link>
   </nav>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { mapState } from "vuex";
+import Logo from "@/components/Logo.vue";
+import { FormOutput } from "@/interfaces/interfaces";
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { Logo },
+  computed: {
+    ...mapState(["cart"]),
+    cartCount(): number {
+      return this.cart.reduce(
+        (currentItem: number, nextItem: FormOutput) =>
+          (currentItem += nextItem.count),
+        0
+      );
+    },
+  },
+});
 </script>
 
 <style scoped>
